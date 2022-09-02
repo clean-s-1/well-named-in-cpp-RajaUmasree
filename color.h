86e18c7937c495377674e3761ddf0736a883cc52
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-09-02 07:21:56 +0530

    Create color.h

---
+++ color.h
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+namespace color {
+    
+ enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+ 
+  const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+  };
+    
+  int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+        
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+class colorPair {
+    public:
+       MajorColor majorColor;
+       MinorColor minorColor;
+       colorPair (MajorColor major, MinorColor minor) : majorColor{major} , minorColor{minor} {}
+       MajorColor getMajorColor();
+       MinorColor getMinorColor();
+       string makepair();
+};
+
+colorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+
+}
